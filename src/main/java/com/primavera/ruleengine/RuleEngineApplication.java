99f81071d0e93a58a0e7233bf24b618c77f7a114
Oytun Oztoprak <oytun.oztoprak@gmail.com>
2021-10-20 15:54:00 +0300

    small fixes

--- src/main/java/com/primavera/ruleengine/RuleEngineApplication.java
+++ src/main/java/com/primavera/ruleengine/RuleEngineApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 @SpringBootApplication
 public class RuleEngineApplication implements CommandLineRunner {
